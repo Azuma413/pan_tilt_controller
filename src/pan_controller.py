18eb947698639ef87d4667a8ba683e84ff4d7279
Azuma413 <hirekatsu0523@gmail.com>
2023-03-14 10:50:24 +0900

    a

--- src/pan_controller.py
+++ src/pan_controller.py
@@ -12,27 +12,26 @@ vertical_NoP = 800 #vertical number of pixels
 vertical_AoV = 57 #vertical angle of view
 horizontal_NoP = 1280
 horizontal_AoV = 86
-speed = 1
+speed = 1 #
 
 def sendPanTiltPos():
-  d_time = 0.02
-  jt_pt = JointTrajectory()
-  jt_pt.header.frame_id = "pantilt_link"
-  jt_pt.joint_names[0] ="tilt"	  #チルトモータID:1
-  jt_pt.joint_names[1] ="pan"		#パンモータID:2
-  jt_pt.header.stamp = rospy.Time.now()
-  #jt_pt.points[0].positions[0] = present_pos["tilt"] + target_pos["tilt"]  #チルトモータ目標角度
-  jt_pt.points[0].positions[0] = 0                                          #縦方向を固定
-  jt_pt.points[0].positions[1] = present_pos["pan"] + target_pos["pan"]     #パンモータ目標角度
-  jt_pt.points[0].time_from_start = rospy.Duration(d_time)  #遷移時間
-  pub1.publish(jt_pt)
+    d_time = 0.02
+    jt_pt = JointTrajectory()
+    jt_pt.header.frame_id = "pantilt_link"
+    jt_pt.joint_names[0] ="tilt"	  #チルトモータID:1
+    jt_pt.joint_names[1] ="pan"		#パンモータID:2
+    jt_pt.header.stamp = rospy.Time.now()
+    #jt_pt.points[0].positions[0] = present_pos["tilt"] + target_pos["tilt"]  #チルトモータ目標角度
+    jt_pt.points[0].positions[0] = 0                                          #縦方向を固定
+    jt_pt.points[0].positions[1] = present_pos["pan"] + target_pos["pan"]     #パンモータ目標角度
+    jt_pt.points[0].time_from_start = rospy.Duration(d_time)  #遷移時間
+    pub1.publish(jt_pt)
 
 def sendRoverTwist():
     rover_data = Twist()
-    rover_data.linear = [0.0, 0.0, 0.0]
-    rover_data.angular = [0.0, 0.0, 0.0]
-    rover_data.linear[1] = target_pos["pan"] * speed
-    rover_data.angular[2] = target_pos["tilt"]
+    rover_data.linear.x = 0.0
+    rover_data.linear.y = target_pos["pan"] * speed
+    rover_data.angular.z = target_pos["tilt"]
     pub2.publish(rover_data)
 
 def callback1(data):
@@ -49,9 +48,8 @@ if __name__ == "__main__":
     present_pos = {"pan":0.0, "tilt":0.0}
     target_pos = {"pan":0.0, "tilt":0.0}
     rospy.init_node('pan_controller')
-    rospy.wait_for_service('/dynamixel_workbench/dynamixel_command')
     pub1 = rospy.Publisher("/dynamixel_workbench/joint_trajectory", JointTrajectory, queue_size=1)
-    pub2 = rospy.Publisher("/hogehoge", Twist, queue_size=1)
+    pub2 = rospy.Publisher("mouse_vel", Twist, queue_size=1)
     sub1 = rospy.Subscriber("Detected_Object_Position", Point, callback1, queue_size=10)
     sub2 = rospy.Subscriber("/joint_states", JointState, callback2, queue_size=10)
     try:
