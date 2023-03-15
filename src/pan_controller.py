a38da1dfc596779e215f95d40607be84952516e6
Azuma413 <hirekatsu0523@gmail.com>
2023-03-15 12:16:35 +0900

    a

--- src/pan_controller.py
+++ src/pan_controller.py
@@ -9,17 +9,17 @@ from geometry_msgs.msg import Point
 from sensor_msgs.msg import JointState
 from geometry_msgs.msg import Twist
 from std_msgs.msg import Float32MultiArray
+import numpy as np
 
 vertical_NoP = 800 #vertical number of pixels
 vertical_AoV = 57 #vertical angle of view
 horizontal_NoP = 1280
 horizontal_AoV = 86
-speed = 1 #
-
-#def sendPanTiltPos():
-#    global present_pos
+speed = 1 
+pool_size = 100
+target_pan_pool = np.zeros((pool_size))
+target_tilt_pool = np.zeros((pool_size))
     
-
 def sendRoverTwist():
     rover_data = Twist()
     rover_data.linear.x = 0.0
@@ -29,8 +29,14 @@ def sendRoverTwist():
 
 def callback1(data):
     global target_pos
-    target_pos["tilt"] = math.radians(data.x / horizontal_NoP * horizontal_AoV)
-    target_pos["pan"] = math.radians(data.y / vertical_NoP * vertical_AoV)
+    global target_pan_pool
+    global target_tilt_pool
+    np.append(target_pan_pool, data.y)
+    np.append(target_tilt_pool, data.x)
+    np.delete(target_pan_pool, 0)
+    np.delete(target_tilt_pool, 0)
+    target_pos["tilt"] = math.radians(np.mean(target_tilt_pool) / horizontal_NoP * horizontal_AoV)
+    target_pos["pan"] = math.radians(np.mean(target_pan_pool) / vertical_NoP * vertical_AoV)
 
 def callback2(data):
     global present_pos
@@ -39,7 +45,9 @@ def callback2(data):
     array = [0.0, 0.0]
     array[0] = present_pos["pan"] + target_pos["pan"]
     array[1] = present_pos["tilt"] + target_pos["tilt"]
+    #array[1] = 0.0
     array_forpub = Float32MultiArray(data=array)
+    print("present:{}/{}\ntarget:{}/{}".format(present_pos["pan"]/3.14*180, present_pos["tilt"]/3.14*180, target_pos["pan"]/3.14*180, target_pos["tilt"]/3.14*180))
     pub1.publish(array_forpub)
     
 if __name__ == "__main__":
@@ -51,10 +59,10 @@ if __name__ == "__main__":
     sub1 = rospy.Subscriber("Detected_Object_Position", Point, callback1, queue_size=10)
     sub2 = rospy.Subscriber("/dynamixel_workbench/joint_states", JointState, callback2, queue_size=10)
     try:
-        while not rospy.is_shutdown():
-            #sendPanTiltPos()
-            sendRoverTwist()
-            print("present:{}/{}\ntarget:{}/{}".format(present_pos["pan"]/3.14*180, present_pos["tilt"]/3.14*180, target_pos["pan"]/3.14*180, target_pos["tilt"]/3.14*180))
-            rospy.Rate(50).sleep()
+        #while not rospy.is_shutdown():
+        #    #sendPanTiltPos()
+        #    sendRoverTwist()
+        #    rospy.Rate(50).sleep()
+        rospy.spin()
     except KeyboardInterrupt:
         pass
